4d0b1995907d439557afdab632331dbd476eefa2
jachym.putta <jachym.putta@u.yale-nus.edu.sg>
2023-04-10 14:21:49 +0800

    Major progress, scrolling + icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'header-gray': '#333',
+      },
+    },
   },
   plugins: [],
 }
